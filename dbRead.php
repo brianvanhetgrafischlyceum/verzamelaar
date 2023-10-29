acc8aed4796342fd7c25214c0adcb6c9ecfe9b0f
brianvanhetgrafischlyceum <120646159+brianvanhetgrafischlyceum@users.noreply.github.com>
2023-10-29 20:47:36 +0100

    final final
    final

--- dbRead.php
+++ dbRead.php
@@ -1,19 +1,11 @@
 <?php
-/* 
 
-
-
-
-
-
-$servername = "localhost";
-$username = "89607admin";
+$servername = "089607.nl.mysql";
+$username = "089607_nl89607beroeps";
 $password = "89607Admin1";
-$dbname = "89607beroeps";
+$dbname = "089607_nl89607beroeps";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-Query de database
 
 $sql = "SELECT spelnaam, spelbeschrijving, spelprijs FROM spellen";
 $result = $conn->query($sql);
@@ -24,42 +16,6 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $games[] = $row;
     }
-}
-
-// Sluit de database-verbinding
-$conn->close();
-
-// Stuur de gegevens naar de webpagina als JSON
-header('Content-Type: application/json');
-echo json_encode($games);
-
-
-
-
-
-
-
-
-
-
-*/
-
-// Database-verbinding met SQLite
-$dbfile = 'beroeps.db';
-$conn = new SQLite3($dbfile);
-
-if (!$conn) {
-    die("Kan geen verbinding maken met de database: " . $conn->lastErrorMsg());
-}
-
-// Query de database
-$sql = "SELECT spelnaam, spelbeschrijving, spelprijs FROM spellen";
-$result = $conn->query($sql);
-
-// Verwerk de gegevens
-$games = array();
-while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    $games[] = $row;
 }
 
 // Sluit de database-verbinding
